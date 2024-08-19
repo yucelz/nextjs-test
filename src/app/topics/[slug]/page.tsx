910e8a93d18b91fb7e51c31aa430720374504961
yucelz <yucelz@gmail.com>
2024-08-19 19:14:44 +0100

    post form

--- src/app/topics/[slug]/page.tsx
+++ src/app/topics/[slug]/page.tsx
@@ -1,4 +1,21 @@
-export default function TopicShowPage(){
-    return <div>TopicShowPage</div>
+import PostCreateForm from "@/components/post/post-create-form";
+
+interface TopicShowPageProps {
+  params: {
+    slug: string;
+  };
 }
 
+export default function TopicShowPage({ params }: TopicShowPageProps) {
+  const { slug } = params;
+  return (
+    <div className="grid grid-cols-4 gap-4 p-4">
+      <div className="col-span-3">
+        <h1 className="text-2xl font-bold mb-2">{slug}</h1>
+      </div>
+      <div>
+        <PostCreateForm />
+      </div>
+    </div>
+  );
+}
