407fdd9b20a4e939e78df115f2e4314dd67e5876
yucelz <yucelz@gmail.com>
2024-08-19 20:49:28 +0100

    create comment has issue

--- src/app/topics/[slug]/page.tsx
+++ src/app/topics/[slug]/page.tsx
@@ -1,21 +1,27 @@
-import PostCreateForm from "@/components/post/post-create-form";
+import Link from 'next/link';
+import PostShow from '@/components/posts/post-show';
+import CommentList from '@/components/comments/comment-list';
+import CommentCreateForm from '@/components/comments/comment-create-form';
+import paths from '@/paths';
 
-interface TopicShowPageProps {
+interface PostShowPageProps {
   params: {
     slug: string;
+    postId: string;
   };
 }
 
-export default function TopicShowPage({ params }: TopicShowPageProps) {
-  const { slug } = params;
+export default async function PostShowPage({ params }: PostShowPageProps) {
+  const { slug, postId } = params;
+
   return (
-    <div className="grid grid-cols-4 gap-4 p-4">
-      <div className="col-span-3">
-        <h1 className="text-2xl font-bold mb-2">{slug}</h1>
-      </div>
-      <div>
-        <PostCreateForm slug={slug} />
-      </div>
+    <div className="space-y-3">
+      <Link className="underline decoration-solid" href={paths.topicShow(slug)}>
+        {'< '}Back to {slug}
+      </Link>
+      <PostShow postId={postId} />
+      <CommentCreateForm postId={postId} startOpen />
+      {/* <CommentList /> */}
     </div>
   );
 }
