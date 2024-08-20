a2f6eb624321c51d5528b2329e736d03e01ab7f8
yucelz <yucelz@gmail.com>
2024-08-20 13:58:57 +0100

    cache ++

--- src/db/queries/comments.ts
+++ src/db/queries/comments.ts
@@ -1,21 +1,25 @@
 import type { Comment } from '@prisma/client';
+import { cache } from 'react';
 import { db } from '@/db';
 
-
 export type CommentWithAuthor = Comment & {
-    user: { name: string | null; image: string | null }
-}
+  user: { name: string | null; image: string | null };
+};
 
-export function fetchCommentsByPostId(postId: string): Promise<CommentWithAuthor[]> {
+export const fetchCommentsByPostId = cache(
+  (postId: string): Promise<CommentWithAuthor[]> => {
+    console.log('Making a query!');
+
     return db.comment.findMany({
-        where: { postId },
-        include: {
-            user: {
-                select: {
-                    name: true,
-                    image: true
-                }
-            }
-        }
-    })
-}
+      where: { postId },
+      include: {
+        user: {
+          select: {
+            name: true,
+            image: true,
+          },
+        },
+      },
+    });
+  }
+);
